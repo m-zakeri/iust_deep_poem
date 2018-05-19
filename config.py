d0d999a12eb504f927bc144a275eca67d8d50f14
Morteza Zakeri <m-zakeri@live.com>
2018-05-19 16:26:33 +0430

    iust_deep_poem (poem generator) initial commit. 13970230

---
+++ config.py
@@ -0,0 +1,17 @@
+
+dataset_config = {
+    'file_fromat': 'txt',  # ['pdf', 'xml', 'html', 'png', 'txt']
+    'dataset_size': 'small',  # ['small', 'medium', 'large']
+
+    'small_training_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+    'small_validation_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+    'small_testing_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+
+    'medium_training_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+    'medium_validation_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+    'medium_testing_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+
+    'large_training_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+    'large_validation_set_path': '/dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+    'large_testing_set_path': './dataset/ferdosi/ferdosi_poem_1_trainset.txt',
+}
